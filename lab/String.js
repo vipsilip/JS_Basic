7bef4048509a217aafdc07e3446acddd972dea97
Le Nguyen Thanh Hai <34795665+vipsilip@users.noreply.github.com>
2025-02-24 13:20:31 +0700

    abc

--- lab/String.js
+++ lab/String.js
@@ -16,18 +16,18 @@
 // Lưu ý: không được sử dụng for, chỉ được dùng hàm replace()
 
 function removeVowel(str) {
-  if (str === '') return '';
+  if (str === "") return "";
   return str
-    .replace('u', '')
-    .replace('e', '')
-    .replace('o', '')
-    .replace('a', '')
-    .replace('i', '')
+    .replace("u", "")
+    .replace("e", "")
+    .replace("o", "")
+    .replace("a", "")
+    .replace("i", "")
     .trim();
 }
-console.log(removeVowel(''));
-console.log(removeVowel('say hello'));
-console.log(removeVowel('     say hello     '));
+console.log(removeVowel(""));
+console.log(removeVowel("say hello"));
+console.log(removeVowel("     say hello     "));
 
 // Format số giây luôn hiển thị 2 chữ số
 // Viết hàm formatSeconds(seconds) nhận vào là số giây (0 <= seconds < 60) và trả về chuỗi luôn có 2 chữ số của số giây.
@@ -51,13 +51,13 @@ console.log(removeVowel('     say hello     '));
 // Happy coding!
 
 function formatSeconds(seconds) {
-  if (seconds < 0 || seconds >= 60) return '';
+  if (seconds < 0 || seconds >= 60) return "";
   //if...else
   return seconds < 10 ? `0${seconds}` : `${seconds}`;
 }
 
 function formatSeconds(seconds) {
-  if (seconds < 0 || seconds >= 60) return '';
+  if (seconds < 0 || seconds >= 60) return "";
   //slice
   return `0${seconds}`.slice(-2);
 }
@@ -90,7 +90,7 @@ console.log(formatSeconds(20));
 // Happy coding!
 
 function formatTime(seconds) {
-  if (seconds < 0 || seconds > 86400) return '';
+  if (seconds < 0 || seconds > 86400) return "";
   const HH = Math.trunc(seconds / 3600);
   const MM = Math.trunc((seconds - HH * 3600) / 60);
   const SS = seconds - (HH * 3600 + MM * 60);
@@ -105,7 +105,7 @@ console.log(formatTime(539));
 console.log(formatTime(4256));
 
 function formatTime(seconds) {
-  if (seconds < 0 || seconds > 86400) return '';
+  if (seconds < 0 || seconds > 86400) return "";
   const secondToHour = 3600;
   const secondToMinute = 60;
   const h = Math.trunc(seconds / secondToHour);
@@ -121,3 +121,42 @@ console.log(formatTime(0));
 console.log(formatTime(9));
 console.log(formatTime(539));
 console.log(formatTime(4256));
+
+// Kiểm tra URL có sử dụng phương thức bảo mật
+// Viết hàm isSecureUrl(url) nhận vào là một url và trả về true nếu url có sử dụng phương thức bảo mật, ngược lại trả về false.
+
+// Quy ước URL được xem là có sử dụng phương thức bảo mật nếu bắt đầu bằng:
+
+// https
+
+// wss
+
+// Tạm thời bỏ qua việc kiểm tra url có hoàn chỉnh hay không, chỉ quan tâm phương thức bảo mật.
+
+// Ví dụ:
+
+// isSecureUrl('http://abc.com') --> false vì bắt đầu bằng http, ko phải là https
+
+// isSecureUrl('https://ezfrontend.com') --> true
+
+// isSecureUrl('wss://chat.ezfrontend.com') --> true
+
+// isSecureUrl('ws://chat.abc.com') --> false
+
+// Viết bằng 2 hướng tiếp cận khác nhau:
+
+// isSecureUrlV1(url) Sử dụng indexOf()
+
+// isSecureUrlV2(url) Sử dụng startsWith()
+
+// Happy coding!
+
+function isSecureUrlV1(url) {
+  if (url === "") return "";
+  //indexOf()
+  return url.indexOf("https") || url.indexOf("wss");
+}
+console.log(isSecureUrlV1("http://abc.com"));
+console.log(isSecureUrlV1("https://abc.com"));
+console.log(isSecureUrlV1("wss://abc.com"));
+console.log(isSecureUrlV1("ws://abc.com"));
